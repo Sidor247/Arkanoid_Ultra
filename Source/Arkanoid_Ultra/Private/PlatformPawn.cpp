07f51a206d26ef3428629796cfac530a78d83d33
Igor Lebedev <gogon65@mail.ru>
2024-09-05 20:41:57 +0300

    Editor startup object fix

--- Source/Arkanoid_Ultra/Private/PlatformPawn.cpp
+++ Source/Arkanoid_Ultra/Private/PlatformPawn.cpp
@@ -27,7 +27,7 @@ void APlatformPawn::Tick(float DeltaSeconsds)
 
 APlatformPawn::APlatformPawn()
 {
-	//	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(FName("StaticMesh"));
 	RootComponent = StaticMeshComponent;
